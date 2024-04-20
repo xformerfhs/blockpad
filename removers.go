c7c75213fdb96a153ae0ccf79243d7fa651f54e4
Frank Schwab <frank.schwab@live.de>
2024-04-20 18:46:01 +0200

    change: Put example in example_test file

--- removers.go
+++ removers.go
@@ -17,14 +17,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-// Author: Frank Schwab
-//
-// Version: 1.0.0
-//
-// Change history:
-//    2024-04-19: V1.0.0: Created.
-//
 
 package blockpad
 
